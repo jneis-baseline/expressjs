278642ef151666301f446b9e86ef37f27140366e
Josue Neis <jneis@users.noreply.github.com>
2016-01-04 16:54:13 -0200

    getting started

--- helloapp/app.js
+++ helloapp/app.js
@@ -3,7 +3,25 @@ var app = express();
 
 app.get('/', function(request, response) {
     response.send('Hello world!');
-});
+})
+
+.post('/', function(request, response) {
+    response.send('Got a POST request');
+})
+
+.put('/user', function(request, response) {
+    response.send('Got a PUT request at /user');
+})
+
+.delete('/user', function(request, response) {
+   response.send('Got a DELETE request at /user'); 
+})
+
+// path is relative to launch directory
+.use(express.static('public'))
+
+// absolute path + mount path 'files/'
+.use('/files', express.static(__dirname + '/static'));
 
 var server = app.listen(3000, function() {
    var host = server.address().address;
